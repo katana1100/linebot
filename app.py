f2bf7d5ebbcb22a2e479d32b8a6067e207a88035
katana1100 <43977859+katana1100@users.noreply.github.com>
2018-10-24 15:58:27 +0800

    test

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 from flask import Flask, request, abort
 import os
 import sys
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import (
-    MessageEvent, TextMessage,
-    TextSendMessage,
-    ImageSendMessage,
-    TemplateSendMessage,
-    ButtonsTemplate, ConfirmTemplate, CarouselTemplate,
-    PostbackTemplateAction, MessageTemplateAction, URITemplateAction,
-    CarouselColumn
+from linebot import (
+    LineBotApi, WebhookHandler
 )
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = 'fcpqJvbf0JbHmEWbUyzMKqwoe2rGrty1UZ6uG9SaAuRZO6P/CU5wuU/f/RR5SRQiwRtoaOaugFAFHEwjpoEDSg0NqI6fF0mLy+BYLsae1upv0LqjmDfWfdf/4q8Fy1bZLkBfya2uHjCImkfFpASPfAdB04t89/1O/w1cDnyilFU='
-SECRET = '497bc40894efdc359bf439bdfc61d34f'
+ACCESS_TOKEN= os.environ['ACCESS_TOKEN']
+SECRET= os.environ['CHANNEL_SECRET']
 
 # Channel Access Token
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(ACCESS_TOKEN)
 # Channel Secret
-handler = WebfromhookHandler(SECRET)
+handler = WebhookHandler(SECRET)
 
 
 @app.route("/")
@@ -47,12 +43,12 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='This is a rare pepe and feels post only in 100 000 posts. If u dont upvote this in 10 seconds you will have bad luck and suffer from the dank meme curse!')
+    message = TextSendMessage(text='Nice to meet you!')
     message2 = TextSendMessage(text=(event.source.user_id)) #reply userid
-    image_url = 'https://i.imgur.com/bCBt6ga.jpg'
-    message3 = ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
     line_bot_api.reply_message(event.reply_token, message)  
-
+    # message = TextSendMessage(text=event)
+    print(event)
+    
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
